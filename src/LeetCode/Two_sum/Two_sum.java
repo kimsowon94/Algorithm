47738ffc623819336f9344367f529dbc388cd722
kimsowon <57133268+kimsowon94@users.noreply.github.com>
2022-05-20 17:45:12 +0900

    Two Sum 완료

--- src/LeetCode/Two_sum/Two_sum.java
+++ src/LeetCode/Two_sum/Two_sum.java
@@ -2,7 +2,23 @@ package LeetCode.Two_sum;
 
 public class Two_sum {
     public int[] twoSum(int[] nums, int target) {
-        int [] result = twoSum(nums, target);
-        return result;
-    }
+
+         int index = nums.length;
+         int num;
+         int [] result = new int[2];
+
+         for (int i = 0; i < index ; i++) {
+             num = nums[i];
+
+             for (int j = 0; j < index ; j++) {
+                 if (i != j) {
+                     if (num + nums[j] == target) {
+                         result[0] = i;
+                         result[1] = j;
+                     }
+                 }
+             }
+         }
+         return result;
+     }
 }
