ce458c2336d8eedbf64b9180c2ac9edde89c1262
kimsowon <57133268+kimsowon94@users.noreply.github.com>
2022-05-20 14:53:57 +0900

    first Commit

---
+++ src/LeetCode/Two_sum/Two_sum.java
@@ -0,0 +1,8 @@
+package LeetCode.Two_sum;
+
+public class Two_sum {
+    public int[] twoSum(int[] nums, int target) {
+        int [] result = twoSum(nums, target);
+        return result;
+    }
+}
